7b253341fd0992ef8cdeceb69323f1232bf0ef09
Josue <juanchorivera100@gmail.com>
2020-12-04 22:25:59 -0600

    Primer commit

---
+++ src/main/java/com/desarrollo/josue/platzimarket/PlatziMarketApplication.java
@@ -0,0 +1,15 @@
+package com.desarrollo.josue.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatziMarketApplication.class, args);
+        
+    }
+
+    
+}
